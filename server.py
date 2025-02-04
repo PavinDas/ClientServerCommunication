45ab5f3c283335d953f646075eab3130dd0df568
Pavin Das <pavindas007@gmail.com>
2025-02-04 18:47:45 +0530

    Add: server close when client enter 'exit'

--- server.py
+++ server.py
@@ -1,24 +1,24 @@
 import socket
 import time
 
-def start_server(host='127.0.0.1', port=65432):
+def start_server(host='127.0.0.1', port=5050):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((host, port))
         server_socket.listen()
         print(f"Server listening on {host}:{port}")
 
-        while True:
-            conn, addr = server_socket.accept()
-            with conn:
-                print(f"Connected by {addr}")
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    message = data.decode()
-                    print(f"Received: {message}")
-                    log_message_to_file(addr[0], message) 
-                    conn.sendall(data)  
+        conn, addr = server_socket.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    print("Connection closed by client")
+                    break
+                message = data.decode()
+                print(f"Received: {message}")
+                log_message_to_file(addr[0], message) 
+                conn.sendall(data)  
 
 def log_message_to_file(ip, message):
     with open("network_logs.txt", "a") as log_file:
